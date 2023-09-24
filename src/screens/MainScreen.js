9e7579be37f1ae0e0dc7237ece6307198090e857
ywonchae1 <ywonchae62@gmail.com>
2023-09-24 16:55:00 +0900

    #4 feat: 스택 분리

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MainScreen() {
+  //구현필요
+  return (
+    <View>
+      <Text>MainScreen</Text>
+    </View>
+  )
+}
