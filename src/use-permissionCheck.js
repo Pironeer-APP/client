e27ac7ef5b4a782531ef93514c5c83c49cadcb5b
Seown Min <nvpz1598@gamil.com>
2023-11-22 21:16:11 +0900

    #215 fix: 공지에서 이미지 안보이는 문제 해결

--- src/use-permissionCheck.js
+++ src/use-permissionCheck.js
@@ -1,5 +1,6 @@
 import {Alert, Platform} from 'react-native';
 import {PERMISSIONS, RESULTS, request} from 'react-native-permissions';
+import {PermissionsAndroid} from 'react-native';
 
 class PermissionCheck {
   cmmDevicePlatformCheck = () => {
@@ -7,28 +8,45 @@ class PermissionCheck {
     if (Platform.OS !== 'ios' && Platform.OS !== 'android') !isUseDevice;
     return isUseDevice;
   };
-
+ 
   cmmReqPermission = async selectImages => {
     if (!this.cmmDevicePlatformCheck()) return;
-    const platformPermissions =
-      Platform.OS === 'ios'
-        ? PERMISSIONS.IOS.PHOTO_LIBRARY
-        : PERMISSIONS.ANDROID.PHOTO_LIBRARY;
-    try {
-      const result = await request(platformPermissions);
-
-      if (result == RESULTS.GRANTED) {
-        console.log('권한이 허용되었습니다.');
-        selectImages();
-      } else {
-        Alert.alert('갤러리 접근 권한을 허용해주세요');
+    if (Platform.OS === 'ios') {
+      const permission = PERMISSIONS.IOS.PHOTO_LIBRARY;
+      try {
+        const result = await request(permission);
+  
+        if (result == RESULTS.GRANTED) {
+          console.log('권한이 허용되었습니다.');
+          selectImages();
+        } else {
+          Alert.alert('갤러리 접근 권한을 허용해주세요');
+        }
+      } catch (err) {
+        Alert.alert('Images permission err');
+        console.warn(err);
       }
-    } catch (err) {
-      Alert.alert('Images permission err');
-      console.warn(err);
+
+
+    } else if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.requestMultiple([PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE, PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+        ]).then((result) => {
+          if (result['android.permission.READ_EXTERNAL_STORAGE'] && result['android.permission.WRITE_EXTERNAL_STORAGE'] === 'granted') {
+            console.log('모든 권한 획득');
+            selectImages();
+          }
+          else {
+            console.log('거절된 권한있음')
+          }
+        })
+      } catch (err) {
+        console.warn(err);
+      }
     }
   };
 }
+
 
 const permissionCheck = new PermissionCheck();
 export default permissionCheck;
--- src/variables.js
+++ src/variables.js
@@ -2,3 +2,8 @@
 // export const _IOS_API_HOST="https://app.pironeer-app.p-e.kr/api"
 export const _ANDROID_AVD_API_HOST='http://10.0.2.2:3000/api';
 export const _IOS_API_HOST="http://127.0.0.1:3000/api"
+
+// export const _ANDROID_AVD_HOST='https://app.pironeer-app.p-e.kr/';
+// export const _IOS_HOST="https://app.pironeer-app.p-e.kr/"
+export const _ANDROID_AVD_HOST='http://10.0.2.2:3000/';
+export const _IOS_HOST="http://127.0.0.1:3000/"
